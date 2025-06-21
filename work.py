a296c5746646986cf44ba64cb8b79039926851e8
sefmax <sefmax7@gmail.com>
2025-06-21 15:33:02 +0300

    FIX

--- work.py
+++ work.py
@@ -1,4 +1,4 @@
-def add_one(x):
-    num = int(''.join(str(digit) for digit in x )) + 1
+def add_one(lst):
+    num = int(''.join(str(digit) for digit in lst )) + 1
     new_lst = [int(digit) for digit in str(num)]
     return  new_lst
